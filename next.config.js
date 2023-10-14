cd544bae9fadeac89db5b769b715d78b889f80c8
Vilhjalmur Arnar Vilhjalmsson <79274024+villi02@users.noreply.github.com>
2023-10-14 12:45:15 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  // Add basePath
+  basePath: '/github-pages',
+}
 
 module.exports = nextConfig
